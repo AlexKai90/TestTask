b93713e740d66b762a1dcfe3a9d7b6a69bc9fd58
AlexKai90 <tp.corsair@gmail.com>
2025-02-17 00:21:19 +0300

    Adding dropdown filter

--- RM/app/index.tsx
+++ RM/app/index.tsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, StyleSheet, FlatList, ActivityIndicator, SafeAreaView } from "react-native";
 import { useEffect, useState } from "react";
 import React from "react";
+import {Picker} from '@react-native-picker/picker';
 
 
 
@@ -8,7 +9,8 @@ const Index = () => {
   const [loading, setLoading] = useState(false);
   const [characters, setCharacters] = useState([]);
   const [nextPage, setNextPage] = useState('https://rickandmortyapi.com/api/character');
-       
+  const [filteredCharacters, setFilterCharacters] = useState([]); 
+  const [selectedCategory, setSelectedCategory] = useState('All'); 
 
   const fetchNextPage = async () => {
     if (loading) {
@@ -23,6 +25,9 @@ const Index = () => {
     setCharacters((existingCharacters) => {
       return [...existingCharacters, ...responseJson.results];
       });
+      setFilterCharacters((existingFilterCharacters) => {
+      return [...existingFilterCharacters, ...responseJson.results];
+      });
     setNextPage(responseJson.info.next);
 
     setLoading(false);
@@ -32,14 +37,53 @@ const Index = () => {
     fetchNextPage();
   }, []);
 
+  useEffect(() => {
+    let filtered;
+    if (selectedCategory === 'All') {
+      setFilterCharacters(characters);
+    } else if(selectedCategory === 'Alive') {
+      filtered = characters.filter(character => character.status === selectedCategory);
+      setFilterCharacters(filtered);
+    } else if(selectedCategory === 'Dead') {
+      filtered = characters.filter(character => character.status === selectedCategory);
+      setFilterCharacters(filtered);
+    } else if(selectedCategory === 'unknown') {
+      filtered = characters.filter(character => character.status === selectedCategory);
+      setFilterCharacters(filtered);
+    } else if(selectedCategory === 'Alien') {
+      filtered = characters.filter(character => character.species === selectedCategory);
+      setFilterCharacters(filtered);
+    } else if(selectedCategory === 'Human') {
+      filtered = characters.filter(character => character.species === selectedCategory);
+      setFilterCharacters(filtered);
+    }
+  }, [selectedCategory, characters]);
+
+  
   return (
     <SafeAreaView>
       <Text style={indexStyles.title}>Rick and Morty character list</Text>
+
+      <View style={indexStyles.picker}>                         // Dropdown filter
+        <Picker
+          selectedValue={selectedCategory}
+          onValueChange={(itemValue) =>
+            setSelectedCategory(itemValue)
+          }>
+          <Picker.Item label="All" value="All" />
+          <Picker.Item label="status: alive" value='Alive'/>
+          <Picker.Item label="status: dead" value="Dead" />
+          <Picker.Item label="status: unknown" value="unknown" />
+          <Picker.Item label="species: alien" value="Alien" />
+          <Picker.Item label="species: human" value="Human" />
+        </Picker>
+      </View>
+      
       <FlatList 
-        data={characters}
+        data={filteredCharacters}
         keyExtractor={item => item.id}
         renderItem={( { item } ) => 
-        <View style={indexStyles.innerContainer}> 
+        <View style={indexStyles.innerContainer} onPress={}> 
           <Image 
           source={{uri: item.image}} 
           style={{width: 300, height:300}}/>
@@ -62,6 +106,7 @@ const indexStyles = StyleSheet.create({
     fontWeight: 'bold',
     padding: 8,
     alignSelf: 'center',
+    marginTop: StatusBar.currentHeight || 0,
   },
  
     innerContainer: {
